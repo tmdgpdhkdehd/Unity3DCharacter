cedc5f4355a7305beafb1d531dd717526b79dc64
Ellieka <42927706+tmdgpdhkdehd@users.noreply.github.com>
2022-03-29 23:21:17 +0900

    Create GameMgr.cs

---
+++ GameMgr.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameMgr : MonoBehaviour
+{
+    public GameObject tank;
+    public GameObject tank1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.LogFormat("{0}", (tank1.transform.position - tank.transform.position).magnitude);
+    }
+}
